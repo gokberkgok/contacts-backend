50525d906222979cc9ac5d918c81c3543854603d
gokberkgok <gokberkgok2004@gmail.com>
2025-07-14 23:24:51 +0300

    add validateToken middleware and currentApi(with middleware param)

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getUsers, getUser, createUser, updateUser, deleteUser, registerUser, loginUser}  = require('../controllers/userController');
-
-router.get('/', getUsers).post('/', createUser)
+const { getUsers, getUser, createUser, updateUser, deleteUser, registerUser, loginUser,currentUser}  = require('../controllers/userController');
+const validateToken = require('../middleware/validateTokenHandler');
+router.get('/', getUsers).post('/', createUser).get('/current', validateToken, currentUser);
 router.put('/:id', updateUser).put('/:id', deleteUser).get('/:id', getUser);
 router.post('/register', registerUser).post('/login', loginUser)
 
