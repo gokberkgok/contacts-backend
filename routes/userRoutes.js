d35134b9440d7961ac792a651c0257e566f6bdf6
gokberkgok <gokberkgok2004@gmail.com>
2025-07-28 19:03:18 +0300

    added validation token to parameters and swagger ui comments

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,58 @@
 const express = require('express');
 const router = express.Router();
-const { getUsers, getUser, createUser, updateUser, deleteUser, registerUser, loginUser,currentUser}  = require('../controllers/userController');
+const { body } = require('express-validator');
+const registerValidation = require('../validators/registerValidator');
+const { 
+     getUsers, 
+     getUser, 
+     createUser,
+     updateUser, 
+     deleteUser, 
+     registerUser, 
+     loginUser,
+     currentUser
+}  = require('../controllers/userController');
+ 
 const validateToken = require('../middleware/validateTokenHandler');
-router.get('/', getUsers).post('/', createUser).get('/current', validateToken, currentUser);
-router.put('/:id', updateUser).put('/:id', deleteUser).get('/:id', getUser);
-router.post('/register', registerUser).post('/login', loginUser)
+//router.use(validateToken);
+
+router.get('/', validateToken, getUsers);
+router.get('/current', validateToken, currentUser);
+router.get('/:id', validateToken,  getUser);
+router.post('/', validateToken, createUser);
+router.put('/:id', validateToken, updateUser);
+router.delete('/:id', validateToken, deleteUser);
+router.post('/register',registerValidation, registerUser);
+router.post('/login', loginUser);
+
+/**
+ * @swagger
+ * /api/users/register:
+ *   post:
+ *     summary: Yeni kullanıcı kaydı
+ *     tags: [User]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - username
+ *               - email
+ *               - password
+ *             properties:
+ *               username:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Kullanıcı oluşturuldu
+ *       400:
+ *         description: Hatalı veri
+ */
 
 module.exports = router;
