05ab78e4d3e6b4258df118899130160824d5d414
gokberkgok <gokberkgok2004@gmail.com>
2025-07-28 19:15:12 +0300

    remove swagger options and added swagger/ files

--- server.js
+++ server.js
@@ -9,32 +9,14 @@ const swaggerUi = require('swagger-ui-express');
 const app = express();
 
 app.use(express.json());    // parsera imkan sağlar client serverdan body parse etmek için
-const swaggerJsDoc = require('swagger-jsdoc');
-
-const swaggerOptions = {
-  swaggerDefinition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Contacts API',
-      version: '1.0.0',
-      description: 'Contacts Backend API - github.com/gokberkgok'
-    },
-  },
-  servers: [
-      {
-        url: "http://localhost:5001",
-      },
-    ],
-  apis: ['./routes/*.js'], // jsdoc yorumları burada
-};
 
 const port = process.env.PORT || 5000;
 
 app.use("/api/contacts", contactRoutes);
 app.use("/api/users", userRoutes);
 
-const swaggerDocument = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const swaggerJsDoc = require('./swagger/swaggerConfig');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJsDoc));
 
 connectDb();
 app.use(errorHandler);
